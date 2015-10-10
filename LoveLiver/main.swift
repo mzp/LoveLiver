bbfe53067c26fb5cf6bbde92b9fb0977184f0a33
mzp <mzpppp@gmail.com>
2015-10-10 13:07:07 +0900

    :shell: add cli handler

--- LoveLiver/main.swift
+++ LoveLiver/main.swift
@@ -8,5 +8,36 @@
 
 import Foundation
 
-print("Hello, World!")
+enum Operation: String {
+    case DumpJPEGMetaData = "jpeg"
+    case DumpMOVMetaData = "mov"
+    case CreateLivePhoto = "livephoto"
+}
 
+let cli = CommandLine()
+let op = EnumOption<Operation>(shortFlag: "o", longFlag: "operation", required: true,
+    helpMessage: "LivePhoto option - jpeg for dump JPEG metadata, mov for dump MOV metadata, livephoto for create LivePhoto")
+let image = StringOption(shortFlag: "i", longFlag: "jpeg", required: false,
+    helpMessage: "Path to the image file.")
+let mov = StringOption(shortFlag: "m", longFlag: "mov", required: false,
+    helpMessage: "Path to the mov file.")
+let output = StringOption(shortFlag: "o", longFlag: "output", required: false,
+    helpMessage: "Path to the output live photo.")
+
+cli.setOptions(op, image, mov, output)
+
+do {
+    try cli.parse()
+} catch {
+    cli.printUsage(error)
+    exit(EX_USAGE)
+}
+
+switch op.value! {
+case Operation.DumpJPEGMetaData:
+    print("dump jpeg metadata")
+case Operation.DumpMOVMetaData:
+    print("dump mov metadata")
+case Operation.CreateLivePhoto:
+    print("generate livephoto")
+}
