df6707dd377887e13c5493a87466c45ec1470875
banjun <banjun@gmail.com>
2016-02-08 23:33:23 +0900

    add empty Cocoa Application target

---
+++ LoveLiver-osx/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LoveLiver-osx
+//
+//  Created by BAN Jun on 2016/02/08.
+//  Copyright © 2016年 mzp. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
